c259bf7a1d52db3953393b496552d3e7b6695509
DannyG17 <33905939+DannyG17@users.noreply.github.com>
2021-02-22 16:23:46 -0500

    Date Schedule

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main extends TimerTask {
+    public void run(){
+        System.out.println("Timer Task executed :: " + new Date() + "::" + Thread.currentThread().getName());
+    }
+    public static void main(String[] args) throws ParseException {
+
+        /*
+        TASKS TO COMPLETE
+
+        Add time scheduling
+        Press different register buttons based on the time according to your schedule
+        batch file ?
+        running on windows pc ?
+
+        if failed (did not authenticate the 2 factor), try again with a lesser time interval
+
+        */
+            Timer timer = new Timer();
+            Task task = new Task();     //from Task class
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyy hh:mm:ss");
+            Date date = sdf.parse("22-02-2021 16:08:00");
+            timer.schedule(task,date);
+
+
+
+
+        }
+
+    }
+
+
+
+
+
---
+++ src/com/company/Task.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class Task extends TimerTask {
+
+    public void run() {
+        System.setProperty("webdriver.chrome.driver", "/Users/dannyg/IdeaProjects/Test_Selenium_Project/chromedriver");
+        WebDriver obj = new ChromeDriver();     // Initialize Chrome Browser
+        obj.manage().window().maximize();       // Maximize Screen
+
+        obj.get("https://wrc.fiu.edu/Program/GetProgramDetails?courseId=3b990f59-63a0-480a-83ab-56f1b0b8e1a5&semesterId=72dc3729-af15-4d58-86ee-7e1aa73e2b50");
+        obj.findElement(By.linkText("Log In")).click();     // Log In Button
+        obj.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);  //wait
+
+        obj.findElement(By.className("loginOption")).click();       // Panther ID button
+       /* obj.findElement(By.id("username")).sendKeys("");    //Username typing (Type Username)
+        obj.findElement(By.id("password")).sendKeys("");   //Password typing (Type Password)
+
+        obj.findElement(By.name("submit")).click();     //Submit log in
+
+        WebElement webElement = obj.findElement(By.id("push"));     // Push Notification sent using JS
+        JavascriptExecutor executor = (JavascriptExecutor) obj;
+        executor.executeScript("arguments[0].click();", webElement);
+
+        obj.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);       //wait
+
+        obj.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div[7]/div[9]/div/div/div/button")).click();  // Choose time(7pm);
+
+        obj.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);       //wait
+
+        obj.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/form[2]/div[2]/button[2]")).click(); // Add to cart
+
+        obj.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        ((JavascriptExecutor) obj).executeScript("scroll(0,400)"); //scrolls page to avoid accepting cookies
+
+        obj.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        obj.findElement(By.id("checkoutButton")).click();       // Checkout button
+
+        obj.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        obj.findElement(By.xpath("//*[@id=\"CheckoutModal\"]/div/div[2]/button[2]")).click();   // Final checkout click finishes here and receipt sent to email*/
+    }
+
+
+
+
+
+}
